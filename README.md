9e320e5eb8b832405e2efea23cc68d968c731e88
Arya Wiranata <wiranata.arya.wiranata@gmail.com>
2019-03-25 15:29:18 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<b>Tugas 2 Pemrograman Berbasis Kerangka Kerja H</b>
+
+Anggota Kelompok
+- Aidil Abdillah Suyudi (511610047)
+- Irham muhammad Fadhil (5116100085)
+- Rahmad Yanuar         (5116100159)
+
+Database yang digunakan antara Server Utama dan Server local yaitu MariaDB
+
+Cara Penggunaan :
+1. Create Tabel Pbkk_2 di Server Utama dan Local
+2. Import pbkk_2.sql di Server Utama dan local
+3. Cek Ip Server Utama dan Local
+4. Ubah host di pool menjadi Ip Utama untuk run di local dan ubah host di pool menjadi Ip Local untuk run di local
+5. Jalankan Xampp
+6. Run menggunakan perintah node ataupun nodemon index.js dilocal dan Server Pusat
+7. Program siap untuk ditambahkan.
+
+Tambahan :
+1. untuk mensinkronisasikan ke dua db masing-masing harus mereload halaman login untuk sinkronisasi log dan reload halaman create akun untuk sinkronisasi username
---
+++ pbkk_2.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2019 at 11:20 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pbkk_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE `log` (
+  `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `log`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
